deaea7a658468dd78d7165d090b7954afff110b2
‏ <caoshumin627019@outlook.com>
2022-09-14 19:29:53 +0800

    update

--- jd/getItemList.js
+++ jd/getItemList.js
@@ -1,8 +1,10 @@
-// Old compatibility code, no longer needed.
-if (window.XMLHttpRequest) { // Mozilla, Safari, IE7+ ...
-    httpRequest = new XMLHttpRequest();
-} else if (window.ActiveXObject) { // IE 6 and older
-    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    function Search(){
+    // Old compatibility code, no longer needed.
+    if (window.XMLHttpRequest) { // Mozilla, Safari, IE7+ ...
+        httpRequest = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE 6 and older
+        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    httpRequest.open('GET', 'itemList.json', true);
+    httpRequest.send();
 }
-httpRequest.open('GET', 'getItemList.js', true);
-httpRequest.send();
