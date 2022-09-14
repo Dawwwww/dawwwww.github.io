19fa7fb992d2d1f4fe87966f0e3193ac9b54674d
‏ <caoshumin627019@outlook.com>
2022-09-14 19:24:59 +0800

    update

---
+++ jd/getItemList.js
@@ -0,0 +1,8 @@
+// Old compatibility code, no longer needed.
+if (window.XMLHttpRequest) { // Mozilla, Safari, IE7+ ...
+    httpRequest = new XMLHttpRequest();
+} else if (window.ActiveXObject) { // IE 6 and older
+    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+}
+httpRequest.open('GET', 'getItemList.js', true);
+httpRequest.send();
